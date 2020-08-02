664478c8a717c6afe928ebe8b2421ed1aeaef32f
Joseph Platta <jwplatta@gmail.com>
2020-08-02 16:51:39 -0700

    fix: remove html extension, port, and host

--- lib/quick_study/app/review_app.rb
+++ lib/quick_study/app/review_app.rb
@@ -1,11 +1,15 @@
 require 'sinatra'
+require 'pry'
 
 class ReviewApp < Sinatra::Base
-  set :bind, '0.0.0.0'
-  set :port, 8888
   set :views, File.expand_path('../views', __FILE__)
 
+  get '/' do
+    erb :index
+  end
+
   get '/review' do
+    binding.pry
     erb :index
   end
   # post '/restart' do
