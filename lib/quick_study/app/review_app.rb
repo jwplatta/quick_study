31ad04c73c067add8e731df835119dffa42e7976
Joseph Platta <jwplatta@gmail.com>
2020-08-15 16:30:51 -0700

    feat: add form for limiting the number of review questions when restarting

--- lib/quick_study/app/review_app.rb
+++ lib/quick_study/app/review_app.rb
@@ -22,7 +22,31 @@ class ReviewApp < Sinatra::Base
     erb :index
   end
 
-  get '/parse' do
+  post '/' do
+    question_count = params[:count].to_i
+
+    question_files = Dir.glob(QuickStudy.configuration.quick_study_dir + '/json/*.json')
+
+    questions = []
+
+    question_files.each do |file|
+      questions << File.open(file, 'r') do |json_file|
+        JSON.parse(json_file.read)
+      end
+    end
+    questions = questions.map { |set| set['questions'] }.flatten
+    questions = questions.map { |q| QuickStudy::ReviewQuestion.new(q.transform_keys(&:to_sym)) }
+
+    @questions = if question_count > 0
+      questions.shuffle.slice(0, question_count)
+    else
+      questions.shuffle
+    end
+
+    erb :index
+  end
+
+  post '/parse' do
     system("quick_study -p")
     puts "Done Parsing\n"
 
